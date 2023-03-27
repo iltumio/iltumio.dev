36f044d11a1f5346ba87eae1fa7a74685a573ffe
Manuel Tumiati <36959970+iltumio@users.noreply.github.com>
2023-03-27 19:53:27 +0200

    chore: update qwik and dependencies. Move to pnpm (#20)
    * chore: update qwik and dependencies. Move to pnpm
    
    * fix: rename eslint from yml to js
    
    * fix: netlify toml
    
    * fix: preview entrypoint
    
    * fix: attempt to fix edge functions in netlify

--- src/components/skill/skill.tsx
+++ src/components/skill/skill.tsx
@@ -1,10 +1,12 @@
-import { component$, JSXChildren } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
 
-type SkillProps = { icon: JSXChildren; name: string };
-export default component$(({ icon, name }: SkillProps) => {
+type SkillProps = { name: string };
+export default component$(({ name }: SkillProps) => {
   return (
     <div class="flex flex-row items-center">
-      <div class="flex p-2">{icon}</div>
+      <div class="flex p-2">
+        <Slot name="icon" />
+      </div>
       <div class="text-bold text-l">{name}</div>
     </div>
   );
--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -9,12 +9,12 @@ type SocialProps = {
   size?: number;
 };
 
-export default component$(({ link, type, size }: SocialProps) => {
-  const Icon = SocialIcons[type];
-  const iconSize = size || 30;
+export default component$((props: SocialProps) => {
+  const Icon = SocialIcons[props.type];
+  const iconSize = props.size || 30;
   return (
     <a
-      href={link}
+      href={props.link}
       target="_blank"
       class="hover:text-cyan-600 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
     >
