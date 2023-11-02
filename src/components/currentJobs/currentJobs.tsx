7d9f13ed49cd00216ecfc47c67174f11e3b8b028
Manuel Tumiati <36959970+iltumio@users.noreply.github.com>
2023-11-02 12:38:32 +0100

    Fleek (#29)
    * chore: move to SSG
    
    * chore: support both netlify and fleek
    
    * chore: fix build scripts
    
    * chore: replace pnpm with npm in scripts for CI
    
    * Fix/job positions (#28)
    
    * fix: update job positions
    
    * fix: job positions

--- src/components/currentJobs/currentJobs.tsx
+++ src/components/currentJobs/currentJobs.tsx
@@ -16,21 +16,21 @@ export default component$(() => {
           to="now"
         />
         <JobPosition
+          name="Bilinear.io"
+          role="Managing Director"
+          description="NFT Marketplace and project launchpad on Linea"
+          image="https://pbs.twimg.com/profile_images/1678784889456295936/Fv-m_jpY_400x400.png"
+          link="https://bilinear.io"
+          from="march. 2023"
+          to="now"
+        />
+        <JobPosition
           name="Satellite.im"
           role="Web3 Advisor"
           description="Multi platform, decentralized and privacy focused messaging application build on top of blockchain, IPFS and peer-to-peer connections"
           image="https://pbs.twimg.com/profile_images/1397668569467531266/47JoP9A2_400x400.jpg"
           link="https://satellite.im"
           from="dec. 2020"
-          to="now"
-        />
-        <JobPosition
-          name="Bcode"
-          role="Web3 Advisor"
-          description="Blockchain focused tech firm that develops many applications to give companies easy access and frictionless onboarding to the blockchain"
-          image="https://bcode.cloud/wp-content/uploads/2022/08/bcode-logo-2.png"
-          link="https://bcode.cloud"
-          from="nov. 2020"
           to="now"
         />
       </div>
